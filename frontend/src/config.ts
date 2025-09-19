d19ac3181d2592a4bc006fe8ac4f4a45060651b9
Paul Huang <paulleehuang@gmail.com>
2025-09-19 07:52:38 -0500

    Fix CORS bug

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = import.meta.env.PROD
+  ? "https://be-j5iytwtx6-paul-huangs-projects.vercel.app"
+  : "http://localhost:3000"
